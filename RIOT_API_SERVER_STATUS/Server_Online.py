a0cba4e346c5949554b572ddd586fd685cbd07f9
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2023-10-27 13:15:18 -0400

    Fixed Server is Online or Offline

--- RIOT_API_SERVER_STATUS/Server_Online.py
+++ RIOT_API_SERVER_STATUS/Server_Online.py
@@ -3,13 +3,11 @@ from .Service_Scrapper import ServiceScrapper
 class ServerStatusChecker:
     def server_online(self):
         service = ServiceScrapper()
-        server_online = False
+        server_online = True
         service_status_list = service.get_status()
         if service_status_list is not None:
             for item in service_status_list:
                 if item['Status'] == 'Offline or Partly Offline':
                     server_online = False  
                     print(f"{item['Service']} is offline or not working correctly")
-                else:
-                    server_online = True  
             return server_online
