bfba2090917af0f7f42d5c0f71b132d225455160
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2023-10-30 17:16:29 -0400

    cleaned files

--- GET_PREVIOUS_GAME_DATA/Get_Match_List.py
+++ GET_PREVIOUS_GAME_DATA/Get_Match_List.py
@@ -8,19 +8,13 @@ class Get_Matches_List:
         api_client = RiotApi()
         puuid = get_match_data.get_puuid(summoner_name)
         valid_matches  = []
-        count = 0
         match_ids = api_client.get_match_ids(puuid)
-        i = 0
-        n = len(match_ids) - 1
-        while count < 1 and n >= 0:
-            match_data = get_match_data.get_match_data(match_ids[n]) 
-
+        for id in match_ids:
+            match_data = get_match_data.get_match_data(id) 
             if get_match_data.is_match_valid(match_data):
                 match_info = {
-                    "match_id": match_ids[n],
+                    "match_id": id,
                     "match_data": match_data
                 }
                 valid_matches.append(match_info)
-                count += 1
-            n -= 1
         return valid_matches
--- League_Connect.py
+++
@@ -1,2 +0,0 @@
-from league_connection import LeagueConnection
-
