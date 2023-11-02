866dec3919801c68b4ae39d29b884e77774a5c05
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2023-11-01 23:38:59 -0400

    added cs per min and gold per min

--- GET_PREVIOUS_GAME_DATA/Get_Match_Data.py
+++ GET_PREVIOUS_GAME_DATA/Get_Match_Data.py
@@ -1,10 +1,11 @@
 from DATA_CLASSES.Participant_Class import Participant
 from UTILITIES.Riot_APIS import RiotApi
+from UTILITIES.Utilities import Utilities
 
 
 class Get_Match_Data:
     def is_match_valid(self, match_data):
-        game_duration = match_data['info']['gameDuration'] / 60
+        game_duration = Utilities.convert_time_to_game_time(match_data['info']['gameDuration'])
         if match_data['info']['queueId'] in RiotApi.RANKED_QUEUE_IDS and game_duration > 15:
             return True
 
@@ -23,3 +24,32 @@ class Get_Match_Data:
         for participant in participants_details:
             participants.append(Participant(participant, summoner_name))
         return participants
+    
+    def cs_per_min(self, match_data, participant_id):
+        participant_data = None
+        game_duration = Utilities.convert_time_to_game_time(match_data['info']['gameDuration'])
+
+        for participant in match_data['info']['participants']:
+            if participant['participantId'] == participant_id:
+                participant_data = participant
+                break
+        total_cs = participant_data['totalMinionsKilled'] + participant_data.get('neutralMinionsKilled', 0)
+        cs_per_minute = total_cs / game_duration
+        return round(cs_per_minute, 2) 
+    
+    def get_gold_per_minute(self, match_data, participant_id):
+        participant_data = None
+        game_duration = Utilities.convert_time_to_game_time(match_data['info']['gameDuration'])
+        for participant in match_data['info']['participants']:
+            if participant['participantId'] == participant_id:
+                participant_data = participant
+                break
+
+        total_gold = participant_data['goldEarned']
+        print(total_gold)
+        gold_per_minute = total_gold / game_duration if game_duration > 0 else 0
+        print(game_duration)
+        return round(gold_per_minute, 2)  # Rounded to two decimal places
+
+       
+        
--- Main.py
+++ Main.py
@@ -25,59 +25,64 @@ for match in match_list_data:
         print(parparticipant)
         if parparticipant.isSummoner == True:
             summoner = parparticipant
-    timeline = get_match_timeline_instance.get_match_timeline(match['match_id'])
-    events = get_match_timeline_instance.get_events(timeline)
+    # timeline = get_match_timeline_instance.get_match_timeline(match['match_id'])
+    # events = get_match_timeline_instance.get_events(timeline)
     
-    kills = get_kills_deaths_assists_instance.get_kills_deaths_assists(events, summoner.id)[0]
-    deaths = get_kills_deaths_assists_instance.get_kills_deaths_assists(events, summoner.id)[1]
-    assists = get_kills_deaths_assists_instance.get_kills_deaths_assists(events, summoner.id)[2]
+    # kills, deaths, assists = get_kills_deaths_assists_instance.get_kills_deaths_assists(events, summoner.id)
 
-    print(f"\n\n\n")
-    print("KILLS")
-    for kill in kills:
-        victim_champion = participants[kill.victim_id].champion
-        assist_champ_ids = kill.assisting_participant_ids
-        assisting_champion = []
-        if assist_champ_ids == 0:
-            print(f"@ {kill.timestamp} {summoner_name} ({participants[kill.killer_id].champion}) killed {victim_champion} with no help! @ {kill.position}")
-        else:
-            for champId in assist_champ_ids:
-                assisting_champion.append(participants[champId].champion)
-            print(f"@ {kill.timestamp} {summoner_name} ({participants[kill.killer_id].champion}) killed {victim_champion} with the help of {assisting_champion}! @ {kill.position}")
-    print(f"\n\n\n")
-    print("DEATHS")
-    for death in deaths:
-        killed_by_champion = participants[death.killer_id].champion
-        assist_champ_ids = death.assisting_participant_ids
-        assisting_champion = []
-        if assist_champ_ids == 0:
-            print(f"@ {death.timestamp} {summoner_name} ({participants[death.victim_id].champion}) died to {killed_by_champion} with no help! @ {death.position}")
-        else:
-            for champId in assist_champ_ids:
-                assisting_champion.append(participants[champId].champion)
+
+    # print(f"\n\n\n")
+    # print("KILLS")
+    # for kill in kills:
+    #     victim_champion = participants[kill.victim_id].champion
+    #     assist_champ_ids = kill.assisting_participant_ids
+    #     assisting_champion = []
+    #     if assist_champ_ids == 0:
+    #         print(f"@ {kill.timestamp} {summoner_name} ({participants[kill.killer_id].champion}) killed {victim_champion} with no help! @ {kill.position}")
+    #     else:
+    #         for champId in assist_champ_ids:
+    #             assisting_champion.append(participants[champId].champion)
+    #         print(f"@ {kill.timestamp} {summoner_name} ({participants[kill.killer_id].champion}) killed {victim_champion} with the help of {assisting_champion}! @ {kill.position}")
+    # print(f"\n\n\n")
+    # print("DEATHS")
+    # for death in deaths:
+    #     killed_by_champion = participants[death.killer_id].champion
+    #     assist_champ_ids = death.assisting_participant_ids
+    #     assisting_champion = []
+    #     if assist_champ_ids == 0:
+    #         print(f"@ {death.timestamp} {summoner_name} ({participants[death.victim_id].champion}) died to {killed_by_champion} with no help! @ {death.position}")
+    #     else:
+    #         for champId in assist_champ_ids:
+    #             assisting_champion.append(participants[champId].champion)
             
-            print(f"@ {death.timestamp} {summoner_name} ({participants[death.victim_id].champion}) died to {killed_by_champion} with the help of {assisting_champion}! @ {death.position}")
+    #         print(f"@ {death.timestamp} {summoner_name} ({participants[death.victim_id].champion}) died to {killed_by_champion} with the help of {assisting_champion}! @ {death.position}")
     
-    print(f"\n\n\n")
-    print("ASSISTS")
-    for assist in assists:
-        only_assist = False
-        killed_by_champion = participants[assist.killer_id ].champion
-        victim_champion = participants[assist.victim_id].champion
-        assist_champ_ids = assist.assisting_participant_ids
-        assisting_champion = []
-        if len(assist_champ_ids) == 1:
-            for champId in assist_champ_ids:
-                if champId is summoner.id-1:
-                    only_assist = True
-                else:
-                    assisting_champion.append(participants[champId].champion)
-        else:
-            for champId in assist_champ_ids:
-                if champId is not summoner.id-1:
-                    assisting_champion.append(participants[champId].champion)
-        if only_assist:
-            print(f"@ {assist.timestamp} {summoner_name} ({participants[summoner.id-1].champion}) helped {killed_by_champion} kill {victim_champion} with no more help! @ {assist.position}")
-        else:
-            print(f"@ {assist.timestamp} {summoner_name} ({participants[summoner.id-1].champion}) helped {killed_by_champion} kill {victim_champion} with more help from {assisting_champion}! @ {assist.position}")
+    # print(f"\n\n\n")
+    # print("ASSISTS")
+    # for assist in assists:
+    #     only_assist = False
+    #     killed_by_champion = participants[assist.killer_id ].champion
+    #     victim_champion = participants[assist.victim_id].champion
+    #     assist_champ_ids = assist.assisting_participant_ids
+    #     assisting_champion = []
+    #     if len(assist_champ_ids) == 1:
+    #         for champId in assist_champ_ids:
+    #             if champId is summoner.id-1:
+    #                 only_assist = True
+    #             else:
+    #                 assisting_champion.append(participants[champId].champion)
+    #     else:
+    #         for champId in assist_champ_ids:
+    #             if champId is not summoner.id-1:
+    #                 assisting_champion.append(participants[champId].champion)
+    #     if only_assist:
+    #         print(f"@ {assist.timestamp} {summoner_name} ({participants[summoner.id-1].champion}) helped {killed_by_champion} kill {victim_champion} with no more help! @ {assist.position}")
+    #     else:
+    #         print(f"@ {assist.timestamp} {summoner_name} ({participants[summoner.id-1].champion}) helped {killed_by_champion} kill {victim_champion} with more help from {assisting_champion}! @ {assist.position}")
 
+    
+
+    cs_per_minute = get_match_data_instance.cs_per_min(match["match_data"], summoner.id)
+    gold_per_minute = get_match_data_instance.get_gold_per_minute(match["match_data"], summoner.id)
+    print(f"{cs_per_minute} farm per minute")
+    print(f"{gold_per_minute} gold per minute")
