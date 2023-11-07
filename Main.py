17ef749c59dd550a7ffa0e66c80a2414a65ac6da
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2023-11-06 19:35:59 -0500

    delete unused import

--- Main.py
+++ Main.py
@@ -8,6 +8,3 @@ from Data_To_CSV import Data_To_CSV
 convert_data_to_csv_instance = Data_To_CSV()
 convert_data_to_csv_instance.convert_data_to_csv()
 
-# preprocessor = PreProcessing()
-# data = preprocessor.get_matches_csvs()
-# preprocessor.combine_match_data(data)
